bcb53cd09c9655aee2be4ddcd5d2a4d81289ecf3
Javier Chavez <jachavez6@gmail.com>
2018-04-01 20:44:05 -0400

    Adds functionality to enter new booking and corresponding web page

--- app.py
+++ app.py
@@ -28,6 +28,39 @@ def home():
     return render_template('index.html', bookings=bookings)
 
 
+# renders the page to enter a new booking
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+
+@app.route('/booking', methods=['POST'])
+def insert_booking():
+    g.db = connect_db()
+    film = request.form['film']
+    program = request.form['program']
+    guarantee = request.form['guarantee']
+    percentage = request.form['percentage']
+    start = request.form['start_date']
+    end = request.form['end_date']
+    distributor = request.form['distributor']
+
+    def distrib_to_id():
+        cur = g.db.execute('select distributor_id from distributors where \
+                            distributor = \'{0}\''.format(distributor))
+        dID = [{'id': row[0]} for row in cur.fetchall()]
+        return dID[0]['id']
+
+    cur = g.db.execute('insert into bookings (film, program, guarantee, \
+                       percentage, start_date, end_date, distributor_id) \
+                       values (?, ?, ?, ?, ?, ?, ?)',
+                       (film, program, (int(guarantee) * 100),
+                        (int(percentage) / 100), start, end, distrib_to_id()))
+    g.db.commit()
+    g.db.close()
+    return home()
+
+
 def connect_db():
     return sqlite3.connect(app.database)
 
