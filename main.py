b1272d9b76b71c14319caafe20ddb9d338af3345
Javier Chavez <jachavez6@gmail.com>
2018-04-10 23:13:29 -0400

    Adds code for adding new distributor and saving changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # main.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from app import app
+from forms import DistributorForm
+from models import Distributor, Booking, Payment
 
 
 @app.route('/')
@@ -9,12 +11,37 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/new_distributor')
+@app.route('/new_distributor', methods=['GET', 'POST'])
 def new_distributor():
     """
     Add a new distributor
     """
-    return render_template('new_distributor.html')
+    form = DistributorForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        # save the distributor
+        distributor = Distributor()
+        save_changes(distributor, form, new=True)
+        flash('Distributor added successfully!')
+        return redirect('/')
+
+    return render_template('new_distributor.html', form=form)
+
+
+def save_changes(distributor, form, new=False):
+    """
+    Save the changes to the database
+    """
+    # Get data from form and assign it to the correct attributes
+    # of the SQLAlchemy table object
+
+    distributor.company = form.company.data
+    distributor.payee = form.payee.data
+    distributor.address1 = form.address1.data
+    distributor.address2 = form.address2.data
+    distributor.city = form.city.data
+    distributor.state = form.state.data
+    distributor.zip = form.zip.data
 
 
 @app.route('/new_booking')
