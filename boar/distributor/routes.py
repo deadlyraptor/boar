8fb4b6a73a468dc58b2e8940d2ebe758b94ec9fb
Javier Chavez <jachavez6@gmail.com>
2018-10-13 15:55:53 -0400

    Add custom table for distributors
    This commit also ensures the distributor routes have properly links per 
    the blueprint.

--- boar/distributor/routes.py
+++ boar/distributor/routes.py
@@ -4,7 +4,6 @@ from flask import Blueprint, flash, render_template, redirect, url_for
 from flask_login import login_required, current_user
 from ..models import Distributor
 from .forms import DistributorForm
-from ..tables import Distributors
 from boar import db
 
 distributors_bp = Blueprint('distributors_bp', __name__)
@@ -28,47 +27,27 @@ def new_distributor():
                                   organization_id=current_user.organization_id)
         db.session.add(distributor)
         db.session.commit()
-        flash('Distributor added successfully!')
-        return redirect(url_for('main.index'))
-    return render_template('/distributor/distributor.html', form=form,
+        flash('Distributor added successfully.', 'success')
+        return redirect(url_for('distributors_bp.list_distributors'))
+    return render_template('/distributors/new_distributor.html', form=form,
                            title='New Distributor', heading='New Distributor')
 
 
-@distributors_bp.route('/distributors')
+@distributors_bp.route('/distributor/update/<int:id>', methods=['GET', 'POST'])
 @login_required
-def list_distributors():
-    distributors = Distributor.query.order_by(Distributor.company).filter(
-         Distributor.organization_id == current_user.organization_id).all()
-    if not distributors:
-        flash('No distributors found!')
-        return redirect(url_for('main.index'))
-    else:
-        table = Distributors(distributors)
-        return render_template('/distributor/distributor_table.html',
-                               table=table, title='Distributors',
-                               heading='Distributors',
-                               distributors=distributors)
-
-
-@distributors_bp.route('/distributor/edit/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_distributor(id):
-    distributor = Distributor.query.filter(
-        Distributor.id == id,
-        Distributor.organization_id == current_user.organization_id).first()
+def update_distributor(id):
+    """
+    Updates a distributor
+    """
+    distributor = Distributor.query.filter_by(
+        id=id, organization_id=current_user.organization_id).first_or_404()
     form = DistributorForm(obj=distributor)
     if form.validate_on_submit():
-        distributor.company = form.company.data
-        distributor.payee = form.payee.data
-        distributor.address1 = form.address1.data
-        distributor.address2 = form.address2.data
-        distributor.city = form.city.data
-        distributor.state = form.state.data
-        distributor.zip = form.zip.data
+        form.populate_obj(distributor)
         db.session.commit()
-        flash('Distributor updated successfully!')
+        flash('Distributor successfully updated.', 'success')
         return redirect(url_for('main.index'))
-    return render_template('/distributor/distributor.html', form=form,
+    return render_template('/distributors/new_distributor.html', form=form,
                            title='Edit Distributor',
                            heading='Edit Distributor')
 
@@ -77,10 +56,24 @@ def edit_distributor(id):
 @login_required
 def delete_distributor(id):
     """
-    Delete the item in the database that matches the specified ID in the URL
+    Deletes a distributor
     """
-    distributor = Distributor.query.filter(Distributor.id == id).first()
+    distributor = Distributor.query.filter_by(id=id).first()
     db.session.delete(distributor)
     db.session.commit()
-    flash('Distributor successfully deleted!')
-    return redirect(url_for('list_distributors'))
+    flash('Distributor successfully deleted.', 'success')
+    return redirect(url_for('distributors_bp.list_distributors'))
+
+
+@distributors_bp.route('/distributors')
+@login_required
+def list_distributors():
+    distributors = Distributor.query.order_by(Distributor.company).filter_by(
+         organization_id=current_user.organization_id).all()
+    if not distributors:
+        flash('No distributors found.', 'warning')
+        return redirect(url_for('main.index'))
+    else:
+        return render_template('/distributors/distributor_table.html',
+                               distributors=distributors, title='Distributors',
+                               heading='Distributors')
